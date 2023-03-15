2b84e963aada1735ec62231ae74fef76999727e7
Viachaslau <slava.golovchik1903@gmail.com>
2023-03-15 07:57:28 +0100

    Feedback

---
+++ reviews/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+
+from . import views
+
+
+urlpatterns = [
+    path("", views.ReviewView.as_view()),
+    path("thank-you", views.Report.as_view()),
+    path("reviews", views.ReviewsListView.as_view()),
+    path("reviews/favorite", views.AddFavoriteView.as_view()),
+    path("reviews/<int:pk>", views.ReviewDitailView.as_view())
+]
